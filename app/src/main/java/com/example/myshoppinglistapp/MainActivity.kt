071f200cd64ed461ae42f4d01418f240ccca8c43
ShubhDev2604 <shubhtandon802@gmail.com>
2024-01-21 01:37:09 +0530

    Added an Empty LazyColumn

--- app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -8,13 +8,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
@@ -29,6 +37,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>())}
+                    /* It has made a mutable list of ShoppingItem dataclass named sItems,
+                    so that any change in the shopping list can be reflected on the screen.*/
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.Center
@@ -39,11 +51,25 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text("Add Item!!")
                         }
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize().padding(16.dp) //Pushes button to top as
+                            // Size of screen is fully occupied by LazyColumn()
+                        ){
+                            items(sItems){
 
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
 
